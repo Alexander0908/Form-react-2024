74df8472cfbce530cebbe351c4e11897efaa692d
Alex <example@gmail.com>
2024-01-28 18:43:20 +0200

    translated all text in English

--- form-project/src/components/app-info/app-info.js
+++ form-project/src/components/app-info/app-info.js
@@ -3,9 +3,9 @@ import "./app-info.css";
 const AppInfo = ({increased, employees}) => {
     return (
         <div className="app-info">
-            <h1>Учет сотрудников в компании N</h1>
-            <h2>Общее число сотрудников: {employees}</h2>
-            <h2>Премию получат: {increased}</h2>
+            <h1>Accounting for employees in company N</h1>
+            <h2>Total employee count: {employees}</h2>
+            <h2>Bonus for: {increased}</h2>
         </div>
     )
 }
